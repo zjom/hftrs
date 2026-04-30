874f534f87d1357b838931a9f471b8c888c07d24
Zihan Jin <admin@zihanjin.com>
2026-04-30 13:20:48 +1000

    fix: bench imports

--- benches/itch_bench.rs
+++ benches/itch_bench.rs
@@ -2,7 +2,6 @@ use criterion::{Criterion, Throughput, criterion_group, criterion_main};
 use memmap2::Mmap;
 use std::fs::File;
 use std::hint;
-use std::time::Duration;
 
 #[derive(Default)]
 struct Handler {
@@ -10,7 +9,10 @@ struct Handler {
 }
 
 impl itch5::MessageHandler for Handler {
-    fn on_trade_message(&mut self, _msg: &itch5::TradeMessage) -> std::ops::ControlFlow<()> {
+    fn on_trade_message(
+        &mut self,
+        _msg: &itch5::messages::TradeMessage,
+    ) -> std::ops::ControlFlow<()> {
         self.trades += 1;
         std::ops::ControlFlow::Continue(())
     }
