83abb9ffe1a72bcd5da8834d2739b9071f1218e4
Zihan Jin <admin@zihanjin.com>
2026-05-04 17:22:23 +1000

    feat(itch5): make read_u48 const

--- itch5/src/messages.rs
+++ itch5/src/messages.rs
@@ -2486,10 +2486,10 @@ impl From<u8> for OpenEligibilityStatus {
 
 // -- private utils
 #[inline]
-fn read_u48(bytes: &[u8; 6]) -> u64 {
-    let mut b = [0u8; 8];
-    b[2..].copy_from_slice(bytes);
-    u64::from_be_bytes(b)
+const fn read_u48(bytes: &[u8; 6]) -> u64 {
+    u64::from_be_bytes([
+        0, 0, bytes[0], bytes[1], bytes[2], bytes[3], bytes[4], bytes[5],
+    ])
 }
 
 #[cfg(test)]
